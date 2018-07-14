6863a72da615cda4226acd4a17d3f24d25eeb747
kevinsun <sunjavakai2010@163.com>
2018-07-14 14:56:11 +0800

    v1

---
+++ src/main/java/com/kevinsun/corejava/kevinsun_corejava/App.java
@@ -0,0 +1,13 @@
+package com.kevinsun.corejava.kevinsun_corejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
